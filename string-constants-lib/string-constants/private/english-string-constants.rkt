25d1c1cc3821d1462bd02fc9a37c12cf5b695bfd
Mike Sperber <sperber@deinprogramm.de>
2016-07-29 20:05:53 +0200

    enable/disable tests also works for DMdA teaching languages

--- string-constants-lib/string-constants/private/english-string-constants.rkt
+++ string-constants-lib/string-constants/private/english-string-constants.rkt
@@ -1656,7 +1656,7 @@ please adhere to these guidelines:
   (test-engine-enable-tests "Enable Tests")
   (test-engine-disable-tests "Disable Tests")
   (test-engine-enable-disable-tests-only-in-teaching-languages
-   "The Enable/Disable Tests menu item is used only to disable and enable the HtDP teaching languages support for testing. See the “Show Details” section of the language dialog for controlling the submodules that are run in The Racket Language.")
+   "The Enable/Disable Tests menu item is used only to disable and enable the HtDP and DMdA teaching languages support for testing. See the “Show Details” section of the language dialog for controlling the submodules that are run in The Racket Language.")
 
   (test-engine-ran-1-test "Ran 1 test.")
   (test-engine-ran-1-check "Ran 1 check.")
