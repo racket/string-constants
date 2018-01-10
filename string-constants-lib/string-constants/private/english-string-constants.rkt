cc2e36db3e75413fcdbc997576cbe0b9552796a4
Leif Andersen <leif@leifandersen.net>
2018-01-10 17:54:46 -0500

    Update strings for converting from text to wxme file formats.

--- string-constants-lib/string-constants/private/english-string-constants.rkt
+++ string-constants-lib/string-constants/private/english-string-constants.rkt
@@ -495,7 +495,11 @@ please adhere to these guidelines:
  
  ;;; save file in particular format prompting.
  (save-as-plain-text "Save this file as plain text?")
+ (save-as-binary-format "Convert this file to a DrRacket specific format to keep non-text elements?")
  (save-in-drs-format "Save this file in drscheme-specific non-text format?")
+ (keep-format "Keep (may lose data)")
+ (convert-format "Convert (recommended)")
+ (dont-save "Don't Save")
  (yes "Yes")
  (no "No")
  
