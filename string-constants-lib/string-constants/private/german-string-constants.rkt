ad33a39d2bfd4d5d6c3d824e1f9c4e7955c00e58
Mike Sperber <sperber@deinprogramm.de>
2021-03-28 16:26:51 +0200

    Sync German string constants with latest

--- string-constants-lib/string-constants/private/german-string-constants.rkt
+++ string-constants-lib/string-constants/private/german-string-constants.rkt
@@ -852,15 +852,30 @@
  (autosave-recover "Zurückholen")
  (autosave-unknown-filename "<<unbekannt>>")
 
+   ;; the ~a is replaced with a path
+  (autoload-file-changed-on-disk/with-name
+   "Die Datei:\n  ~a\nwurde auf der Platte geändert. Wollen Sie"
+   " den Puffer-Inhalt überschreiben, so dass er zur Datei auf der Platte passt?")
+  ;; One file per line is appended to the end of this message
+  (autoload-files-changed-on-disk/with-name
+   "Manche Dateien wurden auf der Platte geändert. Wollen Sie deren Puffer-Inhalte"
+   " überschreiben, so dass sie zu den Dateien auf der Platte passen?\n\nGeänderte Dateien:")
+  ;; the ~a is replaced with a path
+  (autoload-file-changed-on-disk-editor-dirty/with-name
+   "Die Datei:\n  ~a\nwurde auf der Platte geändert und ist editiert worden."
+   " Wollen Sie den Puffer-Inhalt überschreiben, so dass er zur Datei auf der Platte passt?")
+  ;; One file per line is appended to the end of this message
+  (autoload-files-changed-on-disk-editor-dirty/with-name
+   "Manche Dateien wurden auf der Platte geändert und sind editiert worden. Wollen Sie deren Puffer-Inhalte"
+   " überschreiben, so dass sie zu den Dateien auf der Platte passen?\n\nGeänderte Dateien:")
+  ;; One file per line is appended to the end of this message
+  (autoload-files-changed-on-disk-editor-dirty&clean/with-name
+   "Manche Dateien wurden auf der Platte geändert und sind editiert worden. Wollen Sie deren Puffer-Inhalte"
+   " überschreiben, so dass sie zu den Dateien auf der Platte passen?\n\nGeänderte Dateien (◇ bedeutet, dass die Datei editiert wurde):")
+  ;; a specialized version of dont-ask-again-always-current
+  (dont-ask-again-always-current/clean-buffer
+   "Nicht noch einmal fragen bei Dateien ohne ungespeicherte Editierungen (immer diese Auswahl verwendenden)")
 
-   ;;; autoloading
-  (autoload-file-changed-on-disk
-   "Die Datei auf der Platte hat sich geändert."
-   " Editor-Inhalt durch Datei auf der Platte ersetzen?")
-  (autoload-file-changed-on-disk-editor-dirty
-   "Die Datei hat sich auf der Platte geändert."
-   " Editor-Inhalt durch Datei auf der Platte ersetzen?"
-   " \n\nDabei könnten Änderungen verlorengehen.")
   (autoload-automatically-reload "Geänderte Dateien automatisch neu laden")
   (autoload-when-the-editor-isnt-dirty "Wenn der Editor nicht geändert wurde")
   (autoload-never-revert "Niemals")
@@ -1943,7 +1958,22 @@
    " Standardpfad in PATH wurde für alle Benutzer konfiguriert, indem die Datei"
    " ~a hinzugefügt wurde und auf ~a zeigt. Sie können dies rückgängig machen, indem"
    " Sie ~a löschen.")
+  (adding-racket/bin-no-paths.d
+   "Es war nicht möglich, racket zum Pfad hinzuzufügen, weil /etc/paths.d nicht"
+   " existieren.\n\nEs könnte sein, dass folgender Befehl um Terminal funktioniert, um das"
+   " Verzeichnis zu erzeugen:\n"
+   "    sudo mkdir /etc/paths.d\nDann nochmal versuchen.")
+  (added-racket/bin-to-path/windows
+   "Es sollte jetzt möglich sein, racket und die anderen Tools von der"
+   " Kommandozeile aus zu benutzen.\n\nDer"
+   " Standard-%PATH% wurde konfiguriert, indem der Registry-Eintrag"
+   " HKEY_CURRENT_USER\\Environment\\Path"
+   " auf ~a und ~a zeigt.")
+  (didnt-add-racket/bin-to-path/unix
+   "DrRacket kann den Pfad unter Unix nicht verändern, aber falls Sie wissen,"
+   " wie Sie PATH selbst ändern können,"
+   " fügen Sie\n\n  ~a\n\n"
+   " hinzu.")
   (add-racket/bin-to-path "Kommandozeile für Racket konfigurieren…") ;; menu item label
-
   )
 
